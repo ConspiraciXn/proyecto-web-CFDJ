b640cb4f251390fd67a3f06aefbae01d52b2f4e7
Jared Soto Lizama <jared.sl@icloud.com>
2021-05-02 22:33:03 -0400

    Alerta inicio de sesion
    Alerta inicio de sesion.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+/* Alertas de acciones ejecutadas correctamente. */
+function iniciarSesion() {
+    /* A futuro añadir método de inicio de sesión. */
+    toastr.success('', '¡Has iniciado sesión correctamente!');
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+}
+}
